82e14a0702f5ea0914e6886777cef8f3d8575cec
Chuan "Carl" Lin <86574582+Zeivene@users.noreply.github.com>
2024-01-26 20:28:31 +0400

    Initial Commit

---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Chuan Lin on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
